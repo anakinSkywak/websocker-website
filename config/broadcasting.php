85d8b8727db97046c04c16a57613b540aaf372e0
anakinSkywak <tratra1711@gmail.com>
2024-11-18 10:57:21 +0700

    cấu hình websocket

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,6 +38,10 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
+                // 'encrypted' => true,
+                'host' => '127.0.0.1',
+                'post' => 6001,
+                'schema' => 'https',
             ],
         ],
 
